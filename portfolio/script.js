4797b0339dad30e66c4cc73374109805f6cee594
DmitriyPlyshevskiy <proffront@gmail.com>
2022-05-13 19:05:44 +0300

    feat: added script for menu burger

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,7 +1,16 @@
 const hamburger = document.querySelector('.hamburger');
+const navOpen = document.querySelector('.nav');
+const navElements = document.querySelectorAll('.nav-items');
 
 function openCloseMenu() {
     hamburger.classList.toggle('is-active');
+    navOpen.classList.toggle('nav-open');
 }
 
 hamburger.addEventListener('click', openCloseMenu);
+
+function closeMenu() {
+    navOpen.classList.remove('nav-open');
+}
+
+navElements.addEventListener('click', closeMenu);
